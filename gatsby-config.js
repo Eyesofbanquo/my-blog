8d25cc3f29be7115aeca01666264f0e6635f73ad
Markim <ms79723@gmail.com>
2020-06-13 23:02:57 -0500

    Update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `BlogofBanquo`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `This is for Megan's eyes only 😠`,
     author: `@gatsbyjs`,
     email: `markim@donderapps.com`,
   },
